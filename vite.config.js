a23745f79c78e760634cbae47511c1b164c26d30
Vijay <vijayr8106@gmail.com>
2026-06-29 11:38:54 +0530

    Configure GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Invoice/',   // <-- Add this line
   plugins: [react()],
   server: {
     port: 3000,
